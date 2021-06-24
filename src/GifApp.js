1cfc787742a4c564088caab29f11b1e1cd3a8f96
José Carlos Palma <palmahn@gmail.com>
2021-06-23 22:03:04 -0600

    Agregar una nueva categoría.

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifApp = () => {
 
+    // const categories = ['Dragon Ball', 'One Punch', 'Akame Ga Kill'];
+    const [categories, setCategories] = useState(['Dragon Ball', 'One Punch', 'Akame Ga Kill']);
+
+    const handleAdd = () => {
+        setCategories(cats => [...cats, 'Hi-Man']);
+    };
 
     return (
         <>
             <h2 className="mt-3">| Gif App</h2>
             <hr />
+            <button className="btn btn-primary" onClick={handleAdd}>Agregar</button>
+            <ol className="mt-2">
+                {
+                    categories.map(cat => {
+                        return <li key={cat}>{cat}</li>;
+                    })
+                }
+            </ol>
         </>
     );
 };
