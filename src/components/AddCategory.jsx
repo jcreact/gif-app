60b9da0b20719aef0fe611a677b186f7c391e168
José Carlos Palma <palmahn@gmail.com>
2021-06-24 17:19:56 -0600

    Documentación del componente AddCategory.jsx

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,15 +1,34 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Formulario (con un input) para agregar una nueva categoría a la lista.
+ *
+ * @param {React.Dispatch<React.SetStateAction<string[]>>} props.setCategories función para agregar nuevas categorías.
+ * @returns {JSX.Element} Renderización HTML del componente.
+ *
+ * @author José Palma
+ * @version 1.0.0
+ */
 export const AddCategory = ({ setCategories }) => {
     const [category, setCategory] = useState('');
 
-    const handleChange = (e) => {
-        setCategory(e.target.value);
+    /**
+     * Maneja el evento 'onChange' del 'input'.
+     *
+     * @param {React.ChangeEvent<HTMLInputElement>} evt Evento OnChange del Input.
+     */
+    const handleChange = (evt) => {
+        setCategory(evt.target.value);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    /**
+     * Maneja el evento 'onSubmit' del formulario.
+     *
+     * @param {React.FormEvent<HTMLFormElement>} evt Evento OnSubmit del formulario.
+     */
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
         if (category && category.trim()) {
             setCategories((cats) => [category, ...cats]);
             setCategory('');
@@ -18,18 +37,19 @@ export const AddCategory = ({ setCategories }) => {
 
     return (
         <>
-            <form onSubmit={handleSubmit} className="mt-3">
+            <form onSubmit={(evt) => handleSubmit(evt)} className="mt-3">
                 <input
                     className="form-control"
                     type="text"
                     value={category}
-                    onChange={handleChange}
+                    onChange={(evt) => handleChange(evt)}
                 />
             </form>
         </>
     );
 };
 
+// Especificación de tipos de las propiedades.
 AddCategory.propTypes = {
     setCategories: PropTypes.func.isRequired,
 };
