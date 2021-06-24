de0608c80cb0335567fd18174d77a4be25907cd4
José Carlos Palma <palmahn@gmail.com>
2021-06-24 15:50:25 -0600

    Documentación del componente GifGridItem.

--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Tarjeta para muestrar un gif.
+ *
+ * @param {string} props.url Es la URL de la imagen o gif (requerido).
+ * @param {string} props.title Es el título de la imagen o gif (requerido).
+ * @param {string} props.username Es el creador de la imagen o gif.
+ * @returns {JSX.Element} Renderización HTML del componente.
+ *
+ * @author José Palma
+ * @version 1.0.0
+ */
 export const GifGridItem = ({ url, title, username }) => {
     return (
         <article className="col animate__animated animate__fadeIn">
@@ -17,12 +28,14 @@ export const GifGridItem = ({ url, title, username }) => {
     );
 };
 
+// Especificación de tipos de las propiedades.
 GifGridItem.propTypes = {
     url: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     username: PropTypes.string,
 };
 
+// Valor por defecto de las propiedades.
 GifGridItem.defaultProps = {
     username: 'Desconocido',
 };
