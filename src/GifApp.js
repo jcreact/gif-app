babb248ca5e05ef6dc7b3700a3037bdffa586e85
José Carlos Palma <palmahn@gmail.com>
2021-06-23 21:42:28 -0600

    Creación del GifApp.

---
+++ src/GifApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GifApp = () => {
+
+
+    return (
+        <>
+            <h2 className="mt-3">| Gif App</h2>
+            <hr />
+        </>
+    );
+};
+
+export default GifApp;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import GifApp from './GifApp';
+
 
 ReactDOM.render(
-    <>
-        <h1>Gif App</h1>
-    </>,
+    <GifApp />,
     document.getElementById('root')
 );
